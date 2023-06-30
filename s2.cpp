8d46402193b5d57bcbd156d36919661eb84a7f86
advancedprogramminch <128412264+advancedprogramminch@users.noreply.github.com>
2023-06-30 14:56:57 +0330

    Add files via upload

---
+++ s2.cpp
@@ -0,0 +1,13 @@
+// Header Files
+#include <iostream>
+
+// namespace help you use pre defined functions and class
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   cout << "Hello World"; // prints Hello World
+   int i;
+   cin>>i;
+   return 0;
+}
